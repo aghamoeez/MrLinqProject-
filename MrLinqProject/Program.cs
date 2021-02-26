f8d5bd6465389d1a895520c7b2337b30edb6bc39
none <moeez_agha@outlook.com>
2021-02-25 19:03:24 -0800

    Add project files.

---
+++ MrLinqProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+List<int> data = new List<int>(){1,2,3,4,5,6,7,8,9,10 };
+
+
+
+
+Console.WriteLine("************Query syntax**********");
+var Querysyntax = from nextDoorCoder in data
+                  where nextDoorCoder > 3
+                  select nextDoorCoder;
+
+foreach (var item  in Querysyntax)
+{
+
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("***********method Syntax***********");
+var methodSyntax = data.Where(methodData => methodData > 3);
+
+foreach (var item in methodSyntax)
+{
+
+    Console.WriteLine(item);
+}
+
+
+
+Console.WriteLine("************Mixed syntax**********");
+var mixedSyntax = (from nextDoorCoder in data
+                  select nextDoorCoder).Where(c=> c > 3);
+
+foreach (var item in mixedSyntax)
+{
+
+    Console.WriteLine(item);
+}
+
+
+
+
+
+
+
+
